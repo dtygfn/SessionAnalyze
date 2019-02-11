b32c84ff3a534e570a89987cae29e57070e00c34
dtygfn <1214948483@qq.com>
2019-02-11 10:24:50 +0800

    广告点击流量实时统计

--- src/main/scala/spark/ad/AdClickRealTimeStatSpark.scala
+++ src/main/scala/spark/ad/AdClickRealTimeStatSpark.scala
@@ -9,14 +9,16 @@ import kafka.serializer.StringDecoder
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.streaming.dstream.{DStream, InputDStream}
 import org.apache.spark.streaming.kafka.KafkaUtils
-import org.apache.spark.streaming.{Seconds, StreamingContext}
+import org.apache.spark.streaming.{Durations, Seconds, StreamingContext}
 import spark.util.SparkUtils
 import util.DateUtils
 import java.util
 
 import dao.factory.DAOFactory
-import domain.{AdBlacklist, AdStat, AdUserClickCount}
+import domain._
 import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.{Row, SparkSession}
+import org.apache.spark.sql.types.{DataTypes, StructField, StructType}
 
 /**
   * 广告点击流量实时统计
@@ -31,8 +33,6 @@ import org.apache.spark.rdd.RDD
   * 7、使用窗口操作，对最近1小时的窗口内的数据，计算出各广告每分钟的点击量，实时更新到数据库
   */
 object AdClickRealTimeStatSpark extends Serializable {
-
-
 
   def main(args: Array[String]): Unit = { // 模板代码
     val conf = new SparkConf()
@@ -75,7 +75,8 @@ object AdClickRealTimeStatSpark extends Serializable {
     val adRealTimeStatDStream = calcuateRealTimeStat(filteredAdRealTimeLogDStream)
     // 业务二：统计每天各省top3热门广告
     calculateProvinceTop3Ad(adRealTimeStatDStream)
-
+    // 业务三：统计各广告最近1小时内的点击量趋势
+    calculateClickCountByWindow(filteredAdRealTimeLogDStream);
   }
 
   /**
@@ -341,7 +342,150 @@ object AdClickRealTimeStatSpark extends Serializable {
     *
     * @param adRealTimeStatDStream
     */
-  def calculateProvinceTop3Ad(adRealTimeStatDStream: DStream[(String, Long)]): Unit = ???
+  def calculateProvinceTop3Ad(adRealTimeStatDStream: DStream[(String, Long)]): Unit = {
+    // 把adRealTimeStatDStream数据封装到Row
+    // 封装后的数据是没有city字段的
+    val rowDStream = adRealTimeStatDStream.transform(rdd=>{
+      // 把rdd数据格式整合为：<yyyyMMdd_province_adId, clickCount>
+      val mappedRDD = rdd.map(tup=>{
+        val keySplited = tup._1.split("_")
+        val date = keySplited(0)
+        val province = keySplited(1)
+        val adId = keySplited(3).toLong
+
+        val clickCount = tup._2
+        val key = date+"_"+province+"_"+adId
+        (key,clickCount)
+      })
+      // 将mappedRDD的clickCount以省份进行聚合, 得到省份对应的点击广告数
+      val dailyAdClickCountByProvinceRDD = mappedRDD.reduceByKey(_ + _)
+
+      // 将dailyAdClickCountByProvinceRDD转换为DataFrame
+      // 注册为一张临时表
+      // 通过SQL的方式（开窗函数）获取某天各省的top3热门广告
+      val rowsRDD = dailyAdClickCountByProvinceRDD.map(tup=>{
+        val keySplited = tup._1.split("_")
+        val date = keySplited(0)
+        val province = keySplited(1)
+        val adId = keySplited(2).toLong
+        val clickCount = tup._2
+
+        Row(date,province,adId,clickCount)
+      })
+
+      // 指定schema
+      val schema = StructType(Array(
+        StructField("date", DataTypes.StringType, true),
+        StructField("province", DataTypes.StringType, true),
+        StructField("ad_id", DataTypes.LongType, true),
+        StructField("click_count", DataTypes.LongType, true)
+      ))
+
+      val spark = SparkSession.builder().getOrCreate()
+
+      // 映射成DataFrame
+      val dailyAdClickCountByProvinceDF = spark.createDataFrame(rowsRDD,schema)
+
+      // 生成临时表
+      dailyAdClickCountByProvinceDF.createOrReplaceTempView("tmp_daily_ad_click_count_prov");
+      // 使用HiveContext配合开窗函数，给province打一个行标，统计出各省的top3热门商品
+      val sql =
+        "select " +
+          "date, " +
+          "province, " +
+          "ad_id, " +
+          "click_count " +
+          "from (" +
+          "select date,province,ad_id,click_count," +
+          "row_number() over (partition by province order by click_count desc) rank " +
+          "from tmp_daily_ad_click_count_prov" +
+          ") t " +
+          "where rank <= 3"
+      val provinceTop3DF = spark.sql(sql)
+
+      provinceTop3DF.rdd
+    })
+
+    // 将结果写入到数据库中
+    rowDStream.foreachRDD(rdd => {
+      rdd.foreachPartition(it => {
+        val adProvinceTop3s = new util.ArrayList[AdProvinceTop3]()
+        while (it.hasNext) {
+          val row = it.next();
+          val date = row.getString(0)
+          val province = row.getString(1)
+          val adId = row.getLong(2)
+          val clickCount = row.getLong(3)
+
+          val adProvinceTop3 = new AdProvinceTop3()
+          adProvinceTop3.setDate(date)
+          adProvinceTop3.setProvince(province)
+          adProvinceTop3.setAdid(adId)
+          adProvinceTop3.setClickCount(clickCount)
+
+          val adProvinceTop3DAO = DAOFactory.getAdProvinceTop3DAO()
+          adProvinceTop3DAO.updateBatch(adProvinceTop3s)
+        }
+      })
+    })
+  }
+  /**
+    * 统计各广告最近1小时内的点击量趋势
+    *
+    * @param filteredAdRealTimeLogDStream
+    */
+  def calculateClickCountByWindow(filteredAdRealTimeLogDStream: DStream[(String, String)]): Unit = {
+
+    // 首先把数据整合成：<yyyyMMddHHmm_adId, 1L>
+    val tupDStream = filteredAdRealTimeLogDStream.map(tup=>{
+      // 获取原始数据并切分
+      val logSplited = tup._2.split(" ")
+
+      // 把时间戳调整为yyyyMMddHHmm
+      val timeMinute = DateUtils.formatTimeMinute(new Date(logSplited(0).toLong))
+      val adId = logSplited(4).toLong
+
+      (timeMinute+"_"+adId,1L)
+    })
+
+    // 每一次出来的新的batch，都要获取最近一小时的所有的batch
+    // 然后根据key进行reduceByKey，统计出一小时内的各分钟各广告的点击量
+    val aggDtream = tupDStream.reduceByKeyAndWindow(
+      (x: Long, y: Long) => x + y, Durations.minutes(60), Durations.seconds(10))
+
+    // 结果数据的存储
+    aggDtream.foreachRDD(rdd => {
+      rdd.foreachPartition(it => {
+        val adClickTrends = new util.ArrayList[AdClickTrend]()
+        while (it.hasNext) {
+          val tuple = it.next()
+          val keySplited = tuple._1.split("_")
+
+          // yyyyMMddHHmm
+          val dateMinute = keySplited(0)
+          // yyyyMMdd
+          val date = DateUtils.formatDate(DateUtils.parseDateKey(dateMinute.substring(0, 8)))
+          val hour = dateMinute.substring(8, 10)
+          val minute = dateMinute.substring(10)
+          val adId = keySplited(1).toLong
+          val clickCount = tuple._2
+
+          val adClickTrend = new AdClickTrend()
+          adClickTrend.setDate(date)
+          adClickTrend.setHour(hour)
+          adClickTrend.setMinute(minute)
+          adClickTrend.setAdid(adId)
+          adClickTrend.setClickCount(clickCount)
+
+          adClickTrends.add(adClickTrend)
+        }
+
+        val adClickTrendDAO = DAOFactory.getAdClickTrendDAO();
+        adClickTrendDAO.updateBatch(adClickTrends);
+      })
+    })
+  }
+
 
 
 
